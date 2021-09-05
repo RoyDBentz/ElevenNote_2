b01127904bad3077ce3b0c52f6bcb0fbefb88cc9
RoyDBentz <roydbentz@gmail.com>
2021-09-05 13:15:17 -0400

    NoteCreate completed with personal notes

---
+++ ElevenNote/ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{   /* When creating models think about what needs to be
+     * captured by the model. We know this Note model will
+     * need to create Title, Content, and DateCreated, but
+     * is an id necessary?
+     * In this instance the answer is no because the id will 
+     * be created AFTER the POST request happens, after filling 
+     * out a form with the two properties below.
+     * .Service and .Data layer will work together to take care
+     * of that. */
+    public class NoteCreate
+    {
+        // We can add annotations to require or limit the number of
+        // characters in each field 'among other options as well'
+        [Required]
+        [MinLength(2, ErrorMessage="Please enter at least 2 characters.")]
+        [MaxLength(8000)]
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ ElevenNote/ElevenNote.Models/NoteListItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteListItem // remember to check if you need the class to be public IMMEEDIATELY
+    {
+        /* First Model Creation
+         * This first model created will handle the task of 
+         * collecting property data for a list of notes. When
+         * a user enters information, we will want them to 
+         * enter specific data.When we show information, we 
+         * will want to show specific data in specific views.*/
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+
+        /* these are the properties that will show up in the view
+         * data annotations can be added to these properties.
+         * example below    
+         * uses 'System.ComponentModel.DataAnnotations'*/
+        [Display(Name="Created")]
+        public DateTimeOffset CreatedUtc {get; set;}
+
+
+    }
+}
